8e46de4fedc4af549fcd2a19375d54007d29ac8f
Andika Dayu <dikapolk@gmail.com>
2021-10-04 11:07:16 +0700

    Update for testing

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 
-# from models.databaseLite import databaseLite
+#from models.databaseLite import databaseLite
 
 
 # test database
-# dblite = databaseLite()
+#dblite = databaseLite()
 # print(dblite.insert_getId('tb_scrap', "(NULL,'2021-10-02','1')"))
-# dblite.truncate_database()
+#dblite.truncate_database()
 
 # test shopee detail
-# from models.shopeeDetail import shopeeDetail
+#from models.shopeeDetail import shopeeDetail
 
-
-# detail = shopeeDetail(
-#     "https://shopee.co.id/Osaka-Set-Sprei-dan-Bed-Cover-JTI-157-Jacquard-Tencel-i.8534891.8841313151")
-# print(detail.getData())
+#ids = dblite.insert_getId('tb_scrap', "(NULL,'2021-10-02','1')")
+#detail = shopeeDetail("https://shopee.co.id/Osaka-Set-Sprei-dan-Bed-Cover-JTI-157-Jacquard-Tencel-i.8534891.8841313151")
+#print(detail.getData(ids))
