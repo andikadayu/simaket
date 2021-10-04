8df96262f750636509042684d8c930c15c6b06c4
Andika Dayu <dikapolk@gmail.com>
2021-10-04 11:08:06 +0700

    Update Add data to sqlite

--- models/shopeeDetail.py
+++ models/shopeeDetail.py
@@ -1,6 +1,7 @@
 import requests
 from random import randrange
-
+from models.databaseLite import databaseLite
+import json
 
 class shopeeDetail:
     image = {}
@@ -20,11 +21,13 @@ class shopeeDetail:
     stok = 12
     harga = 12000
     url = ""
+    gambar1 = ""
+    video1 = ""
 
     def __init__(self, url):
         self.url = url
 
-    def getData(self):
+    def getData(self,ids):
         link = self.url.replace('?position=', '.')
 
         links = link.split('-i.')
@@ -45,6 +48,8 @@ class shopeeDetail:
             for ans in jsons['data']['images']:
                 self.image['img'+str(i)] = ans
                 i += 1
+                
+            self.gambar1 = json.dumps(self.image)
 
             self.deskripsi = jsons['data']['description'].replace("'", " ")
             self.catid = jsons['data']['catid']
@@ -53,9 +58,10 @@ class shopeeDetail:
                 self.video = jsons['data']['video_info_list']
             else:
                 self.video = None
+                
+            self.video1 = json.dumps(self.video)
 
             for ansi in jsons['data']['models']:
-                # self.harga
                 prices = str(ansi['price'])
                 self.harga = int(prices[0:-5])
                 self.stok = ansi['stock']
@@ -68,8 +74,11 @@ class shopeeDetail:
                 self.str_liks = "https://shopee.co.id/" + \
                     str(self.name.replace(" ", "-"))+"-i." + \
                     str(self.shopid)+"."+self.itemid
-
-            return None
+                    
+                dblite = databaseLite()
+                
+                dblite.insert_database("tb_shopee","(NULL,'"+str(ids)+"','"+str(self.str_liks)+"','"+str(self.name)+"','"+str(self.deskripsi)+"','"+str(self.catid)+"','0','1','0','1','Baru','"+str(self.gambar1)+"','"+str(self.video1)+"','"+str(self.sku)+"','Aktif','"+str(self.stok)+"','"+str(self.harga)+"','optional')")
+            return self.name
 
         except requests.ConnectionError as error:
             return error
